32b1bd5922ba73f8291c9f3dfcc86490dba866a2
Francesco Stefanni <francesco.stefanni@gizeroenergie.it>
2021-04-28 10:07:58 +0200

    Updated includes

---
+++ src/sensirion-embedded-common-extra.hpp
@@ -0,0 +1,9 @@
+#ifndef SENSERION_EMBEDDED_COMMON_EXTRA_HPP
+#define SENSERION_EMBEDDED_COMMON_EXTRA_HPP
+
+#include <Arduino.h>
+
+void sensirion_set_i2c_implementation(TwoWire & wire);
+void sensirion_set_serial_implementation(HardwareSerial * serial);
+
+#endif
