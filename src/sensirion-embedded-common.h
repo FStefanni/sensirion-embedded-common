5cf352dd8b1ac09dc69b2a7fc820bac7b7ab7918
Francesco Stefanni <francesco.stefanni@gizeroenergie.it>
2021-04-28 10:05:44 +0200

    Updated includes

--- src/sensirion-embedded-common.h
+++ src/sensirion-embedded-common.h
@@ -1,6 +1,16 @@
+#ifndef SENSERION_EMBEDDED_COMMON_H
+#define SENSERION_EMBEDDED_COMMON_H
+
+#include <Arduino.h>
+
 #include "sensirion_common.h"
 #include "sensirion_config.h"
 #include "sensirion_i2c_hal.h"
 #include "sensirion_i2c.h"
 #include "sensirion_shdlc.h"
 #include "sensirion_uart_hal.h"
+
+void sensirion_set_i2c_implementation(TwoWire & wire);
+void sensirion_set_serial_implementation(HardwareSerial * serial);
+
+#endif
